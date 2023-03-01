9ed30ea3f044691eaed5fee9a26be2edcd4dda56
Egor Kuzmichev <yegor.kuzmichev@gmail.com>
2023-03-01 22:46:43 +0400

    Set margin for pdf

--- pdf.mjs
+++ pdf.mjs
@@ -10,5 +10,5 @@ const browser = await puppeteer.launch()
 const page = await browser.newPage()
 
 await page.setContent(html, { waitUntil: 'networkidle0' })
-await page.pdf({ path: 'resume.pdf', format: 'a4', printBackground: true })
+await page.pdf({ path: 'resume.pdf', format: 'a4', printBackground: true, margin: { top: '0.5in', right: '0.5in', bottom: '1in', left: '0.5in' } })
 await browser.close()
